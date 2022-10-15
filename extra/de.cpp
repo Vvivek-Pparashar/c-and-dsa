0a876f4491211c296d278c5700a974c8ade77696
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-10-15 11:43:59 +0530

    updated

--- extra/de.cpp
+++ extra/de.cpp
@@ -1,13 +1,76 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n = 10;
+#define fi                          first
+#define se                          second
+#define ll                          long long
+#define pb                          push_back
+#define po                          pop_back
+#define mp                          make_pair
+#define mod                         1000000007
+#define ull                         unsigned long long
+#define vll                         vector<long long>
+#define endl                        "\n"
+#define test                        int t;  cin>>t; while(t--)
+#define vpll                        vector<pair<long long, long long> >
+#define all(v)                      v.begin(), v.end()
+#define prec(n)                     fixed<<setprecision(n)
+#define bits(n)                     __builtin_popcount(n)
+#define loop(i, n)                  for(int i = 0; i<n; i++)
+#define sort_inc(v)                 sort(all(v));
+#define sort_dec(v)                 sort(all(v), greater<int>());
+#define vivek_is_great              ios_base::sync_with_stdio(false) , cin.tie(NULL);
 
-    long long z = 1;
-    for(int i = 1; i<=n; i++){
-        z = (i*z)%1000000007;
+int solver_dp(int sum_alice, int sum_bob, int count_alice, int count_bob, int i, int n, int a[], int &temp){
+    if(i == (n+1)/2){
+        if(count_alice >= count_bob){
+            if(sum_alice %2) return 0;
+            cout<<"HERE"<<endl;
+            temp = 1;
+            return 1;
+        }
+
+        return 0;
     }
 
-    cout<<z<<endl;
+    if(solver_dp(sum_alice + (a[i]%2), sum_bob, count_alice + 1, count_bob, i+1, n, a, temp)) return 1;
+    if(solver_dp(sum_alice, sum_bob + (a[i]%2), count_alice, count_bob + 1, i+1, n, a, temp)) return 1;
+
+    return 0;
+}
+
+
+void solve(){
+    int n;  cin>>n;
+    int a[n];
+
+    loop(i, n) {
+        cin>>a[i];
+    }
+
+    int temp = 0;
+    solver_dp(0, 0, 0, 0, 0, n, a, temp);
+
+    if(temp) cout<<"Alice"<<endl;
+    else cout<<"Bob"<<endl;
+
+
+
+}
+
+int main(){
+    vivek_is_great
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+
+    test
+    {
+        solve();
+    }
+
+
 }
--- extra/dele.cpp
+++ extra/dele.cpp
@@ -1,26 +1,71 @@
+//Code by Vivek Parashar
+//      linkedin  -> vivek parashar
+
 #include<bits/stdc++.h>
 using namespace std;
 
+#define fi                          first
+#define se                          second
+#define ll                          long long
+#define pb                          push_back
+#define po                          pop_back
+#define mp                          make_pair
+#define mod                         1000000007
+#define ull                         unsigned long long
+#define vll                         vector<long long>
+#define endl                        "\n"
+#define test                        int t;  cin>>t; while(t--)
+#define vpll                        vector<pair<long long, long long> >
+#define all(v)                      v.begin(), v.end()
+#define prec(n)                     fixed<<setprecision(n)
+#define bits(n)                     __builtin_popcount(n)
+#define loop(i, n)                  for(int i = 0; i<n; i++)
+#define sort_inc(v)                 sort(all(v));
+#define sort_dec(v)                 sort(all(v), greater<int>());
+#define vivek_is_great              ios_base::sync_with_stdio(false) , cin.tie(NULL);
+
+
+void solve(){
+    int n;  cin>>n;
+    string s;   cin>>s;
+
+    int temp[n+1];
+
+    loop(i, n+1){
+        if(i>0) temp[i] = (s[i-1] == '1');
+    }
+
+    int cost[n+1] = {0};
+
+    for(int i = n; i>=1; i--){
+        for(int j = i; j<=n; j+=i){
+            if(temp[j]) break;
+            cost[j] = i;
+        }
+    }
+
+    ll count = 0;
+
+    loop(i, n+1){
+        if(!temp[i]) count += cost[i];
+    }
+
+    cout<<count<<endl;
+}
+
 int main(){
-    #ifndef ONLINE_JUDGE
+    vivek_is_great
+#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
-    int t;    cin>>t;
-    while(t--){
-        int n;  cin>>n;
-        int a[n];
 
-        for(int  i = 0; i<n; i++) cin>>a[i];
-        sort(a, a+n);
 
-        bool flag = true;
 
-        for(int  i = 1; i<n; i++){
-            if((a[i] - a[i-1]) > 1) flag = false;
-        }
-
-        if(flag) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
+    test
+    {
+        solve();
     }
+
+
 }
